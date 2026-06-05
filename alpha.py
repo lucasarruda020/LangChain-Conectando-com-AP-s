4a7d6d8129a27876ba855940613a29e723abc029
Lucas Arruda <lucasarruda020@gmail.com>
2026-06-05 17:44:00 -0300

    infelizmente por nao usar um modelo mais avancado com as APIs do openAI nao pude completar a atividade da aula, meu modelo nao consegue aguentar as requisicoes que estou fazendo, porem o codigo esta certo e com certeza usarei num futuro quando for assinante da OpenAI

--- alpha.py
+++ alpha.py
@@ -1,10 +1,10 @@
-from openai import OpenAI
-from dotenv import load_dotenv
-import os
 from langchain_openai import ChatOpenAI
 from langchain.prompts import PromptTemplate
-from langchain_core.output_parsers import StrOutputParser
-
+from langchain_core.output_parsers import JsonOutputParser, StrOutputParser
+from pydantic import Field, BaseModel
+from dotenv import load_dotenv
+from langchain.globals import set_debug
+import os
 
 load_dotenv()
 base_url = os.getenv("base_url")
@@ -12,10 +12,17 @@ api_key_local = os.getenv("api_key")
 api_key = os.getenv("OPENAI_API_KEY")
 
 
-numero_dias = 7
-numero_criancas = 2
-atividade = "Praia"
-prompt = f"Crie um roteiro de viagem de {numero_dias} dias, para uma familia com {numero_criancas} de criancas, que busca atividades como essas {atividade}"
+class Restaurante(BaseModel):
+    cidade:str = Field("A cidade recomendada para visitar")
+    restaurantes:str = Field("Motivo pelo qual restaurante eh recomendado")
+
+parseador_restaurante = JsonOutputParser(pydantic_object=Restaurante)
+
+class Destino(BaseModel):
+    cidade:str = Field("A cidade recomendada para visitar")
+    motivo:str = Field("motivo pelo qual é interessante visitar essa cidade")
+
+parseador_destino = JsonOutputParser(pydantic_object=Destino)
 
 
 modelo = ChatOpenAI(
@@ -24,18 +31,37 @@ modelo = ChatOpenAI(
     api_key=api_key_local,
     temperature=0.5
 )
+
 modelo_de_cidade = PromptTemplate(
     template="""
     sugira uma cidade dado o meu interesse por {interesse}.
+    {formato_de_saida}
     """,
-    input_variables=["interesse"]
+    input_variables=["interesse"],
+    partial_variables={"formato_de_saida": parseador_destino.get_format_instructions()}
 )
 
-cadeia = modelo_de_cidade | modelo | StrOutputParser()
+modelo_de_restaurantes = PromptTemplate(
+    template="""
+    sugira restaurantes populares entre locais em {cidade}.
+    {formato_de_saida}
+    """,
+    partial_variables={"formato_de_saida": parseador_restaurante.get_format_instructions()}
+)
+
+modelo_de_cultura = PromptTemplate(
+    template="""Sugira um passeio cultural em {cidade}"""
+)
+
+cadeia_cidade = modelo_de_cidade | modelo | parseador_destino
+cadeia_restaurante = modelo_de_restaurantes | modelo | parseador_restaurante
+cadeia_cultura = modelo_de_cultura | modelo | StrOutputParser()
+
+cadeia = (cadeia_cidade | cadeia_restaurante | cadeia_cultura)
 
 resposta = cadeia.invoke(
     {
-        "interesse": "praias"
+        "interesse" : "praias"
     }
 )
 
